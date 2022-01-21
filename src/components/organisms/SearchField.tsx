bbbed2c3b3bd4906140e9ac3ad510019715fd2d8
fumi uk <dcfumiya.238@gmail.com>
2022-01-21 09:29:46 +0900

    hotel-demo

---
+++ src/components/organisms/SearchField.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from "react";
+import flex from "styles/utility/flex.module.scss";
+import { SearchButton, TextInput } from "components/atoms";
+import { useStringChangeEvent } from "lib/customHooks";
+import { searchHotels } from "lib/hotels";
+import { Hotels } from "types/hotels";
+
+interface Props {
+  handleHotels: (fetchedHotels: Hotels) => void;
+}
+
+const SearchField: FC<Props> = (props) => {
+  const { handleHotels } = props;
+
+  const [keyword, setKeyword] = useState<string>("");
+  const handleKeyword = useStringChangeEvent(setKeyword);
+
+  return (
+    <div className={flex.flex__row_center}>
+      <TextInput
+        onChange={handleKeyword}
+        value={keyword}
+        type={"text"}
+      />
+      <SearchButton
+        label={"Search"}
+        onClick={async () => {
+          const hotels = await searchHotels(keyword);
+          handleHotels([...hotels]);
+        }}
+      />
+    </div>
+  );
+};
+
+export default SearchField;
