811a07cf1b73055aeb4226112bacf756c32e7877
fumi uk <dcfumiya.238@gmail.com>
2022-03-07 09:16:16 +0900

    hotels-demo-redux

--- src/module/HotelListModule/reducers.ts
+++ src/module/HotelListModule/reducers.ts
@@ -1,3 +1,4 @@
+import { Reducer } from "redux";
 import { HotelAction, Hotels } from "types/hotels";
 import { actionTypes } from "./actions";
 
@@ -5,7 +6,7 @@ export const initialState: Hotels = [{
   hotel: []
 }];
 
-export const hotelReducer = (state: Hotels = initialState, action: HotelAction) => {
+export const hotelReducer: Reducer<Hotels, any> = (state: Hotels = initialState, action: HotelAction) => {
   switch (action.type) {
     case actionTypes.RECEIVE_HOTELS:
       return action.response;
--- src/module/SearchModule/reducers.ts
+++ src/module/SearchModule/reducers.ts
@@ -1,3 +1,4 @@
+import { Reducer } from "redux";
 import { Search, SearchAction } from "types/search";
 import { actionTypes } from "./actions";
 
@@ -5,7 +6,7 @@ export const initialState: Search = {
   keyword: ""
 };
 
-export const searchReducer = (state: Search = initialState, action: SearchAction) => {
+export const searchReducer: Reducer<Search, any> = (state: Search = initialState, action: SearchAction) => {
   switch (action.type) {
     case actionTypes.CHANGE_KEYWORD:
       return {
